0dd797b3c73e2f332aa24ae7842b215e70cabc9b
Sumsam Ali <freelancingwithsac@gmail.com>
2024-05-27 12:48:52 +0500

    updated tests

--- client/__tests__/integration/selenium/selenium.test.js
+++ client/__tests__/integration/selenium/selenium.test.js
@@ -10,7 +10,7 @@ const testPassword = "Sumsam.Ali.189";
 
 beforeAll(async () => {
 	const options = new firefox.Options();
-	options.setBinary("/snap/bin/firefox");
+	options.setBinary("/usr/bin/firefox");
 	options.addArguments("-headless");
 
 	driver = await new Builder()
