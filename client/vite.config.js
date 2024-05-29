dc77fea854ff642c4224a02ee7baebdcfd4e7a83
Sumsam Ali <freelancingwithsac@gmail.com>
2024-05-29 11:05:03 +0500

    updated config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,29 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:3000",
-				changeOrigin: true,
-				secure: false,
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+
+	const API_URL = `${env.VITE_BE_API_URL || "http://localhost:3000"}`;
+	const PORT = `${env.VITE_PORT || "5173"}`;
+
+	return {
+		server: {
+			proxy: {
+				"/api": {
+					target: API_URL,
+					changeOrigin: true,
+					secure: false,
+				},
 			},
 		},
-	},
-	test: {
-		globals: true,
-		environment: "jsdom",
-		setupFiles: "./src/setupTests.js",
-		reporters: ["default", "junit"],
-		outputFile: "test-results.xml",
-	},
-	plugins: [react()],
+		test: {
+			globals: true,
+			environment: "jsdom",
+			setupFiles: "./src/setupTests.js",
+			reporters: ["default", "junit"],
+			outputFile: "test-results.xml",
+		},
+		plugins: [react()],
+	};
 });
