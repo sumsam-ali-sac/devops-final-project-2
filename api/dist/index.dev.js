240250e7fa7c6435f523721ecf666cde025fca1b
Sumsam Ali <freelancingwithsac@gmail.com>
2024-05-29 11:36:47 +0500

    updated serverside code

--- api/dist/index.dev.js
+++ api/dist/index.dev.js
@@ -14,16 +14,29 @@ var _routes = require("./config/routes.js");
 
 var _errorHandler = require("./middleware/errorHandler.js");
 
+var _path = _interopRequireDefault(require("path"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 (0, _dotenv.config)();
 (0, _connectDB.connectDB)();
 var app = (0, _express["default"])();
 (0, _middleware.setupMiddlewares)(app);
-(0, _routes.setupRoutes)(app);
+(0, _routes.setupRoutes)(app); // Path to the static files directory
+
+var distPath = _path["default"].join(__dirname, "../client/dist"); // Serve static files from the distPath directory
+
+
+app.use(_express["default"]["static"](distPath)); // Serve the main index.html file for any route not handled by other routes
+
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", {
+    root: distPath
+  });
+});
 app.use(_errorHandler.errorHandler);
 var server = (0, _http.createServer)(app);
 var PORT = process.env.PORT || 8080;
 server.listen(PORT, function () {
-  console.log("Server is listening on https://helphivebot.azurewebsites.net/:".concat(PORT));
+  console.log("Server is listening on https://helphivebot.azurewebsites.net:".concat(PORT));
 });
--- api/index.js
+++ api/index.js
@@ -5,6 +5,12 @@ import { setupMiddlewares } from "./config/middleware.js";
 import { connectDB } from "./lib/connectDB.js";
 import { setupRoutes } from "./config/routes.js";
 import { errorHandler } from "./middleware/errorHandler.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Use import.meta.url to create the equivalent of __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenvConfig();
 connectDB();
@@ -14,6 +20,17 @@ const app = express();
 setupMiddlewares(app);
 setupRoutes(app);
 
+// Path to the static files directory
+const distPath = path.join(__dirname, "../client/dist");
+
+// Serve static files from the distPath directory
+app.use(express.static(distPath));
+
+// Serve the main index.html file for any route not handled by other routes
+app.get("/*", (req, res) => {
+	res.sendFile("index.html", { root: distPath });
+});
+
 app.use(errorHandler);
 
 const server = createServer(app);
@@ -21,6 +38,6 @@ const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
 	console.log(
-		`Server is listening on https://helphivebot.azurewebsites.net/:${PORT}`
+		`Server is listening on https://helphivebot.azurewebsites.net:${PORT}`
 	);
 });
